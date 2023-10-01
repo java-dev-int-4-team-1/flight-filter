5903906ff06772ab2d5fbdcd04aa971d7efb6c20
Sergei-Gots <Sergei.Gots@yahoo.com>
2023-10-01 20:41:01 +0300

    FlightPrinterImpl reduced

--- src/main/java/com/gridnine/testing/flightfilter/impl/FlightPrinterImpl.java
+++ src/main/java/com/gridnine/testing/flightfilter/impl/FlightPrinterImpl.java
@@ -5,16 +5,18 @@ import com.gridnine.testing.flightfilter.FlightPrinter;
 
 import java.util.List;
 
+import static java.lang.System.out;
+
 public class FlightPrinterImpl implements FlightPrinter {
     @Override
     public void printFlights(String description, List<Flight> flights) {
-        String DELIMITER = "================================================================================";
-        System.out.println(DELIMITER);
-        System.out.println("FLIGHTS with the segments");
-        System.out.println(description);
-        System.out.println(DELIMITER);
-        flights.forEach(System.out::println);
-        System.out.println(DELIMITER);
-        System.out.println();
+        String delimiter = "================================================================================";
+        out.println(delimiter);
+        out.println("FLIGHTS with the segments");
+        out.println(description);
+        out.println(delimiter);
+        flights.forEach(out::println);
+        out.println(delimiter);
+        out.println();
     }
 }
